3a7e7238de6d3bdbe00502d36f68bc469336709a
Billy <billyurrutia@gmail.com>
2019-12-12 14:47:53 -0700

    Testing dev tools in chrome

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Idea Box/i);
+  const linkElement = getByText(/IdeaBox/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Ideas.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import './Ideas.css';
+
+// const Ideas = () => {
+//   return (
+//     <h2>Ideas go here!</h2>
+//   )
+// }
+
+// export default Ideas;
+
+import React from 'react';
+import './Ideas.css';
+
+const Ideas = () => {
+  return 
+}
