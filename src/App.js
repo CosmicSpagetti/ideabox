86db3237dc896c227408c21feed580fb1ee49f7b
Billy <billyurrutia@gmail.com>
2020-02-10 10:23:44 -0700

    adding Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Ideas from './Ideas'
+import Ideas from './Ideas';
+import Form from './Form';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +8,30 @@ class App extends Component {
     super();
     this.state = {
       ideas: [
-        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff'},
-        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying'},
-        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies'},
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
       ]
     }
   }
+
   addIdea = (newIdea) => {
     this.setState({ ideas: [...this.state.ideas, newIdea] });
   }
+
+  deleteIdea = (id) => {
+    console.log(id);
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id);
+
+    this.setState({ ideas: filteredIdeas });
+  }
+
   render() {
     return (
       <main className='App'>
         <h1>IdeaBox</h1>
         <Form addIdea={this.addIdea} />
-        <Ideas ideas={this.state.ideas} />
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} />
       </main>
     )
   }
